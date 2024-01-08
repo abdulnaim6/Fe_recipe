93b77bcf32c0a18210cb84436e961954c8c85a2e
Naim <naimabdul71@gmail.com>
2024-01-08 14:04:15 +0700

    create recipe

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 const Button = (props) => {
   return (
     <React.Fragment>
-      <button type="submit" className="btn btn-warning">
+      <button type="submit" className="btn btn-warning" role="button">
         <a href={props.href}>{props.text}</a>
       </button>
     </React.Fragment>
--- src/Page/Addrecipe/index.jsx
+++ src/Page/Addrecipe/index.jsx
@@ -4,94 +4,158 @@ import Footer from "../../Components/Footer";
 import icon from "../../assets/addrecipe.svg";
 import "./style.css";
 import Button from "../../Components/Button";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const Addrecipe = () => {
+  const [data, setData] = React.useState({
+    name_food: "",
+    picture: "",
+    ingrediens: "",
+    video: "",
+  });
+  console.log(data);
+
+  const [isError, setIsError] = React.useState(false);
+  const [savePicture, setSaveImage] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setData({
+      ...data,
+      [e.target.name]: value,
+    });
+  };
+
+  const handleUpload = (e) => {
+    const uploader = e.target.files[0];
+    setSaveImage(uploader);
+    console.log(uploader);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const productData = new FormData();
+    productData.append("name_food", data.name_food);
+    productData.append("picture", savePicture);
+    productData.append("ingrediens", data.ingrediens);
+    productData.append("video", data.video);
+
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/addrecipe`, productData)
+      .then((response) => {
+        console.log(response);
+
+        Swal.fire({
+          icon: "success",
+          title: "Recipe Posted Successfully!",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      })
+      .catch((err) => {
+        console.log(err.errorMessage);
+        setIsError(true);
+        setErrorMessage("Data Error");
+      });
+  }
+
   return (
-    <>
-      <MyNavbar />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <div className="d-flex justify-content-center align-items-center">
-              <div className="image position-relative">
-                <input
-                  className="form-control opacity-0"
-                  type="file"
-                  id="image"
-                  // onChange={handleImageChange}
-                />
-                <div id="icon">
-                  <img
-                    src={icon}
-                    alt="icon"
-                    style={{ width: 50, height: 50 }}
-                  />
-                  <div className="addimg">Add photo</div>
+    <React.Fragment>
+      {isError ? (
+        <h1>{errorMessage}</h1>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <MyNavbar />
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-12">
+                <div className="d-flex justify-content-center align-items-center">
+                  <div className="image position-relative">
+                    <input
+                      className="form-control opacity-0"
+                      type="file"
+                      id="image"
+                      onChange={handleUpload}
+                    />
+                    <div id="icon">
+                      <img
+                        src={icon}
+                        alt="icon"
+                        style={{ width: 50, height: 50 }}
+                      />
+                      <div className="addimg">Add photo</div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div className="container-fluid my-5">
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center align-items-center">
-            <input
-              style={{ width: "1300px" }}
-              id="title"
-              className="form-control"
-              type="text"
-              placeholder="Title"
-              aria-label="form-control-lg example"
-              // value={title}
-              // onChange={(e) => setTitle(e.target.value)}
-            />
+          <div className="container-fluid my-5">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center align-items-center">
+                <input
+                  style={{ width: "1300px" }}
+                  id="title"
+                  className="form-control"
+                  type="text"
+                  placeholder="Title"
+                  name="name_food"
+                  aria-label="form-control-lg example"
+                  value={data.name_food}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center align-items-center text-start">
-            <textarea
-              style={{ width: "1300px" }}
-              id="ingredients"
-              className="form-control"
-              name="ingredients"
-              placeholder="Ingredients"
-              // value={ingredients}
-              // onChange={(e) => setIngredients(e.target.value)}
-            ></textarea>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center align-items-center text-start">
+                <textarea
+                  style={{ width: "1300px" }}
+                  id="ingredients"
+                  className="form-control"
+                  name="ingrediens"
+                  placeholder="Ingredients"
+                  value={data.ingrediens}
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div className="container-fluid my-5">
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center align-items-center">
-            <input
-              style={{ width: "1300px" }}
-              id="Video"
-              className="form-control"
-              type="text"
-              placeholder="Video"
-              aria-label="form-control-lg example"
-              // value={title}
-              // onChange={(e) => setTitle(e.target.value)}
-            />
+          <div className="container-fluid my-5">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center align-items-center">
+                <input
+                  style={{ width: "1300px" }}
+                  id="Video"
+                  className="form-control"
+                  type="text"
+                  placeholder="Video"
+                  aria-label="form-control-lg example"
+                  name="video"
+                  value={data.video}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="container-fluid my-5">
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center align-items-center">
-            <Button href="#" text="Submit" />
+          <div className="container-fluid my-5">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-center align-items-center">
+                <Button text="Post" type="button" />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <Footer />
-    </>
+          <Footer />
+        </form>
+      )}
+    </React.Fragment>
   );
 };
 
